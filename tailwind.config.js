3c0e452b54ab720e6c754b782d73166f5f7b373a
ron-wollschlaeger <ron.wollschlaeger@frontshape.de>
2024-01-15 12:48:03 +0100

    Upload project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Poppins", "sans-serif"'],
+      },
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      }
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
   plugins: [],
